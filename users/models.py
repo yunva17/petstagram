c69f7ad77e8d2da5861155fc03b133d296d37e94
ji_frozen000110 <answldjs1836@gmail.com>
2021-03-20 21:24:50 +0900

    user model 수정

--- users/models.py
+++ users/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+
+class User(models.Model):
+    """ User Model """
+    email = models.CharField(max_length=50, unique=True, null=True)
+    phone = models.CharField(max_length=50, unique=True, null=True)
+    full_name = models.CharFields(max_length=50)
+    username = models, models.CharField(max_length=50)
+    password = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
